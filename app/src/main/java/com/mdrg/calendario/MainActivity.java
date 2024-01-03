554d7d9102fe3ae53d8cbdccc98deb28556c7f4b
MiltonRamos2002 <milton.ramos954@ist17dejulio.edu.ec>
2024-01-03 11:11:25 -0500

    Calendario

---
+++ app/src/main/java/com/mdrg/calendario/MainActivity.java
@@ -0,0 +1,40 @@
+package com.mdrg.calendario;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CalendarView;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+import java.util.Calendar;
+
+public class MainActivity extends AppCompatActivity {
+    TextView tv;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = findViewById(R.id.t1);
+    }
+
+    public void AbreCalenadario(View view) {
+        Calendar cal = Calendar.getInstance();
+        int anio = cal.get(Calendar.YEAR);
+        int mes = cal.get(Calendar.MONTH);
+        int dia = cal.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog dpd = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int month, int dayOfMonth) {
+                String fecha = dayOfMonth + "/" + month + "/" + year ;
+                tv.setText(fecha);
+
+            }
+        }, dia, mes, anio);
+        dpd.show();
+    }
+}
